42832c2dd0e87a0c8bb82d13c48571b2ea36f3b3
Rich Peeers <richpeers1@gmail.com>
2017-02-06 14:48:17 +0000

    branding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Tasks</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,15 +79,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Tasks
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://richpeers.co.uk" target="_blank">richpeers.co.uk</a>
+                    <a href="https://github.com/richpeers/tasks" target="_blank">Github</a>
+                    <a href="https://twitter.com/richpeers" target="_blank">Twitter</a>
                 </div>
             </div>
         </div>
