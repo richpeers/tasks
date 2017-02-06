d728b909c0b18a66806e0f5eebc1a19f53eb4ed1
Rich Peeers <richpeers1@gmail.com>
2017-02-06 14:09:50 +0000

    services for local env

---
+++ app/Providers/LocalServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LocalServiceProvider extends ServiceProvider
+{
+    protected $providers = [
+        'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider',
+        'Barryvdh\Debugbar\ServiceProvider'
+    ];
+    protected $aliases = [
+        'Debugbar' => 'Barryvdh\Debugbar\Facade'
+    ];
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //register the service providers for local environment
+        if ($this->app->isLocal() && !empty($this->providers)) {
+            foreach ($this->providers as $provider) {
+                $this->app->register($provider);
+            }
+            //register the alias
+            if (!empty($this->aliases)) {
+                foreach ($this->aliases as $alias => $facade) {
+                    $this->app->alias($alias, $facade);
+                }
+            }
+        }
+    }
+}
